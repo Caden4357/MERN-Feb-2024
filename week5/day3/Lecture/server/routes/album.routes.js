3eb0726f488da62d0be3be7a854c5301332809df
Caden Wilcox <wilcox4357@gmail.com>
2024-03-21 19:31:44 -0700

    wk 5 session 3

--- week5/day3/Lecture/server/routes/album.routes.js
+++ week5/day3/Lecture/server/routes/album.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import * as albumController from "../controllers/album.controllers.js";
 const router = Router();
-router.route('/album')
+
+router.route('/albums')
+    .get(albumController.getAllAlbums)
     .post(albumController.createAlbum);
+router.route('/albums/:id')
+    .get(albumController.getOneAlbumById)
+    .delete(albumController.deleteById)
+    .put(albumController.updateAlbumById)
 
 
 export default router;
